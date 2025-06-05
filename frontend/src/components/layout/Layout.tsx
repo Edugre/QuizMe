dba0a7b97e5b5d527d0d9b9d987035aaf7d309a5
Eduardo Goncalvez <eduardogoncalvez00@gmail.com>
2025-06-05 11:37:40 -0400

    Create Navbar

---
+++ frontend/src/components/layout/Layout.tsx
@@ -0,0 +1,15 @@
+import { Children } from "react";
+import { Navbar } from "./Navbar";
+
+interface LayoutProps {
+    children: React.ReactNode;
+}
+
+export default function Layout({children}: LayoutProps) {
+    return (
+        <div className="min-h-screen">
+            <Navbar />
+            <main>{children}</main>
+        </div>
+    );
+}
